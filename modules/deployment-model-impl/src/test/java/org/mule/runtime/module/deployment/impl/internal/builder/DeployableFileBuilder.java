57e52cc3404b7161dd819e19ab2933107f54e56f
pablo.kraan <pablo.kraan@gmail.com>
2017-07-20 08:03:43 -0300

    MULE-12937: Artifacts using heavy package must should not depend on maven for classloader model resolution

--- modules/deployment-model-impl/src/test/java/org/mule/runtime/module/deployment/impl/internal/builder/DeployableFileBuilder.java
+++ modules/deployment-model-impl/src/test/java/org/mule/runtime/module/deployment/impl/internal/builder/DeployableFileBuilder.java
@@ -8,11 +8,21 @@ package org.mule.runtime.module.deployment.impl.internal.builder;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static org.mule.runtime.deployment.model.api.application.ApplicationDescriptor.REPOSITORY_FOLDER;
+import static org.mule.runtime.deployment.model.api.plugin.ArtifactPluginDescriptor.MULE_PLUGIN_CLASSIFIER;
+import static org.mule.runtime.module.deployment.impl.internal.plugin.PluginMavenClassLoaderModelLoader.CLASSLOADER_MODEL_JSON_DESCRIPTOR;
+import static org.mule.runtime.module.deployment.impl.internal.plugin.PluginMavenClassLoaderModelLoader.CLASSLOADER_MODEL_JSON_DESCRIPTOR_LOCATION;
+import static org.mule.tools.api.ContentGenerator.createArtifactClassLoaderModelJsonFile;
+import static org.mule.tools.api.ContentGenerator.createPluginClassLoaderModelJsonFile;
 import org.mule.runtime.core.api.util.StringUtils;
 import org.mule.runtime.module.artifact.builder.AbstractArtifactFileBuilder;
 import org.mule.runtime.module.artifact.builder.AbstractDependencyFileBuilder;
 import org.mule.tck.ZipUtils;
+import org.mule.tools.api.classloader.model.Artifact;
+import org.mule.tools.api.classloader.model.ArtifactCoordinates;
+import org.mule.tools.api.classloader.model.ClassLoaderModel;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
@@ -21,6 +31,8 @@ import java.util.Properties;
 public abstract class DeployableFileBuilder<T extends DeployableFileBuilder<T>> extends AbstractArtifactFileBuilder<T> {
 
   protected Properties deployProperties = new Properties();
+
+  private boolean useHeavyPackage = true;
 
   public DeployableFileBuilder(String id, boolean upperCaseInExtension) {
     super(id, upperCaseInExtension);
@@ -53,9 +65,15 @@ public abstract class DeployableFileBuilder<T extends DeployableFileBuilder<T>> 
     return getThis();
   }
 
+  public T usingLightwayPackage() {
+    useHeavyPackage = false;
+    return getThis();
+  }
+
   @Override
   protected final List<ZipUtils.ZipResource> getCustomResources() {
     final List<ZipUtils.ZipResource> customResources = new LinkedList<>();
+
 
     for (AbstractDependencyFileBuilder dependencyFileBuilder : getAllCompileDependencies()) {
       customResources.add(new ZipUtils.ZipResource(dependencyFileBuilder.getArtifactFile().getAbsolutePath(),
@@ -67,13 +85,73 @@ public abstract class DeployableFileBuilder<T extends DeployableFileBuilder<T>> 
                                                              dependencyFileBuilder.getArtifactFilePomRepositoryPath())
                                                        .toString()));
 
+      if (useHeavyPackage && MULE_PLUGIN_CLASSIFIER.equals(dependencyFileBuilder.getClassifier())) {
+        File pluginClassLoaderModel = createClassLoaderModelJsonFile(dependencyFileBuilder);
+        customResources.add(new ZipUtils.ZipResource(pluginClassLoaderModel.getAbsolutePath(),
+                                                     Paths.get(REPOSITORY_FOLDER,
+                                                               dependencyFileBuilder.getArtifactFileRepositoryFolderPath(),
+                                                               CLASSLOADER_MODEL_JSON_DESCRIPTOR)
+                                                         .toString()));
+      }
     }
 
     customResources.addAll(doGetCustomResources());
+
+    if (useHeavyPackage) {
+      customResources.add(new ZipUtils.ZipResource(getClassLoaderModelFile().getAbsolutePath(),
+                                                   CLASSLOADER_MODEL_JSON_DESCRIPTOR_LOCATION));
+    }
+
     return customResources;
   }
 
+  private File createClassLoaderModelJsonFile(AbstractDependencyFileBuilder dependencyFileBuilder) {
+    ArtifactCoordinates artifactCoordinates =
+        new ArtifactCoordinates(dependencyFileBuilder.getGroupId(), dependencyFileBuilder.getArtifactId(),
+                                dependencyFileBuilder.getVersion(), dependencyFileBuilder.getType(),
+                                dependencyFileBuilder.getClassifier());
+    ClassLoaderModel classLoaderModel = new ClassLoaderModel("1.0", artifactCoordinates);
+
+    List<Artifact> artifactDependencies = new LinkedList<>();
+    List<AbstractDependencyFileBuilder> dependencies = dependencyFileBuilder.getDependencies();
+    for (AbstractDependencyFileBuilder fileBuilderDependency : dependencies) {
+      artifactDependencies.add(getArtifact(fileBuilderDependency));
+    }
+
+    classLoaderModel.setDependencies(artifactDependencies);
+
+    Path repository = Paths.get(getTempFolder(), REPOSITORY_FOLDER, dependencyFileBuilder.getArtifactFileRepositoryFolderPath());
+    if (repository.toFile().exists()) {
+      repository.toFile().delete();
+    } else {
+      if (!repository.toFile().mkdirs()) {
+        throw new IllegalStateException("Cannot create artifact folder inside repository");
+      }
+    }
+
+    return createPluginClassLoaderModelJsonFile(classLoaderModel, repository);
+  }
+
+  private File getClassLoaderModelFile() {
+    ArtifactCoordinates artifactCoordinates = new ArtifactCoordinates(getGroupId(), getArtifactId(), getVersion());
+    ClassLoaderModel classLoaderModel = new ClassLoaderModel("1.0", artifactCoordinates);
+
+    List<Artifact> artifactDependencies = new LinkedList<>();
+    for (AbstractDependencyFileBuilder fileBuilderDependency : getDependencies()) {
+      artifactDependencies.add(getArtifact(fileBuilderDependency));
+    }
+
+    classLoaderModel.setDependencies(artifactDependencies);
+
+    return createArtifactClassLoaderModelJsonFile(classLoaderModel, Paths.get(getTempFolder()));
+  }
+
+  private Artifact getArtifact(AbstractDependencyFileBuilder builder) {
+    ArtifactCoordinates artifactCoordinates =
+        new ArtifactCoordinates(builder.getGroupId(), builder.getArtifactId(), builder.getVersion(), builder.getType(),
+                                builder.getClassifier());
+    return new Artifact(artifactCoordinates, builder.getArtifactFile().toURI());
+  }
+
   protected abstract List<ZipUtils.ZipResource> doGetCustomResources();
-
-
 }
